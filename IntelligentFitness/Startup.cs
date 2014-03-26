2b7f481dbe6e68900b4f85c37bf52af7f32d9b49
Palmermatic <palmermatic@gmail.com>
2014-03-26 11:46:04 -0500

    initial wizard

---
+++ IntelligentFitness/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IntelligentFitness.Startup))]
+namespace IntelligentFitness
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
